18b6298d95cc8bbfbf1b6aa3f3b7a72a277908aa
Agustin Ricardo <agustinricardo14@gmail.com>
2022-02-14 17:00:03 -0300

    'asd'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import React from 'react'
+
 
 function App() {
+  // El primer estado es para crear un estado de tipo array vacío para guardar la lista
+  const [todos, setTodos] = useState([]); 
+  // El segundo estado es para sacar el value del input
+  const [todo, setTodo] = useState('');
+
+  // Función para sacar el value del input
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  }
+
+  // Función donde al completarse el submit del form, se crea un objeto con el value del input
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const newTodo = {
+      id: 1 + Math.random(),
+      text: todo,
+      completed: false
+    }
+
+    // setTodos ejecuta la concatenación del array todos y el nuevo objeto que creamos antes
+    setTodos([...todos].concat(newTodo))
+
+    // Ejecutamos el setTodo con un string vacío para vaciar el input
+    setTodo('')
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <section className='section-body'>
+      <h3>To Do List</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={todo} onChange={handleChange}/>
+          <input type="submit" value="Agregar"/>
+        </form>
+        <div className='list'>
+          {todos.map((todo) => <div key={todo.id}>{todo.text}</div>)}
+        </div>
+      </section>
+    </header>
+  </>
   );
 }
 
